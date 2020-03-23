6c7598badca2cce12438c1ec5ec119ad3a95e2f7
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2020-03-23 16:25:26 +0100

    removed jwt

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Validator\Constraints\Json;
 
 class SecurityController extends AbstractController
 {
@@ -23,7 +25,8 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return new JsonResponse();
     }
 
     /**
